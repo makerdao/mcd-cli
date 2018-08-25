b8da1f3780df9f4d5d01b2fc53a83dd449248160
Mariano Conti <nanexcool@gmail.com>
2018-08-25 17:02:42 -0300

    Nixify installation

---
+++ default.nix
@@ -0,0 +1,28 @@
+{ lib, stdenv, fetchFromGitHub, makeWrapper, glibcLocales
+, bc, coreutils, curl, ethabi, ethsign, git, gnused, jshon, nodejs, perl, solc }:
+
+stdenv.mkDerivation rec {
+  name = "mcd-${version}";
+  version = "0.1.0";
+  src = ./.;
+
+  nativeBuildInputs = [makeWrapper];
+  buildPhase = "true";
+  makeFlags = ["prefix=$(out)"];
+  postInstall = let path = lib.makeBinPath [
+    bc coreutils curl ethabi ethsign git gnused jshon nodejs perl solc
+  ]; in ''
+    wrapProgram "$out/bin/mcd" --prefix PATH : "${path}" \
+      ${if glibcLocales != null then
+        "--set LOCALE_ARCHIVE \"${glibcLocales}\"/lib/locale/locale-archive"
+        else ""}
+  '';
+
+  meta = {
+    description = "Command-line client Multicollateral Dai";
+    homepage = https://github.com/makerdao/mcd-cli/;
+    maintainers = [stdenv.lib.maintainers.dbrock];
+    license = lib.licenses.gpl3;
+    inherit version;
+  };
+}
